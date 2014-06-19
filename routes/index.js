abd6703c19315f78527e3f64291a771366742353
showzeb <showzeb@ip-198-12-148-109.secureserver.net>
2014-06-19 13:55:06 -0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var winston = require('winston');
+
+winston.add(winston.transports.File, { filename: 'somefile.log' });
+/* GET home page. */
+router.get('/', function(req, res) {
+var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+
+//	console.log(ip);
+winston.info(ip);
+  res.render('index');
+});
+
+router.get('/resume', function(req, res) {
+	res.redirect('/resume.pdf');
+});
+
+module.exports = router;
