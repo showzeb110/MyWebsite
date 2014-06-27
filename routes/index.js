bd636bbbe064641c02dd2eb4c3c7fecbdd516167
showzeb110 <ms.ali@mail.utoronto.ca>
2014-06-27 02:09:04 -0400

    Adding huffman article and removing more boiler plate code

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@ var router = express.Router();
 var winston = require('winston');
 
 winston.add(winston.transports.File, { filename: 'somefile.log' });
+
 /* GET home page. */
 router.get('/', function(req, res) {
-var ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
+	var ip = req.headers['x-forwarded-for'] || 
+		req.connection.remoteAddress || 
+		req.socket.remoteAddress ||
+		req.connection.socket.remoteAddress;
 
-//	console.log(ip);
-winston.info(ip);
-  res.render('index');
+	winston.info(ip);
+	res.render('index',{index:'class=active', contact:'', articles:'', projects:''});
 });
 
 router.get('/resume', function(req, res) {
@@ -20,19 +20,23 @@ router.get('/resume', function(req, res) {
 });
 
 router.get('/contact', function(req, res) {
-	res.render('contact');
+	res.render('contact',{index:'', contact:'class=active', articles:'', projects:''});
 });
 
 router.get('/articles', function(req, res) {
-	res.render('articles');
+	res.render('articles',{index:'', contact:'', articles:'class=active', projects:''});
 });
 
 router.get('/projects', function(req, res) {
-	res.render('projects');
+	res.render('projects',{index:'', contact:'', articles:'', projects:'class=active'});
 });
 
 router.get('/articles/2014/new-site', function(req, res) {
-	res.render('articles/2014/new-site');
+	res.render('articles/2014/new-site',{index:'', contact:'', articles:'', projects:''});
+});
+
+router.get('/articles/2014/huffman', function(req, res) {
+	res.render('articles/2014/huffman',{index:'', contact:'', articles:'', projects:''});
 });
 
 module.exports = router;
